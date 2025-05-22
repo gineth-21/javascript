35d483344fecf52135da9f95c86928462f12251a
gineth-21 <gdnovoac@libertadores.edu.co>
2025-05-22 02:13:30 +0000

    Primer avace del relato de Caperucita Roja

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-// Código JavaScript para la historia de Caperucita Roja
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('El proyecto está listo para comenzar');
+    // Registrar el plugin ScrollTrigger
+    gsap.registerPlugin(ScrollTrigger);
+
+    // Animación de Caperucita
+    gsap.to('.caperucita', {
+        x: window.innerWidth - 150, // Mover hasta casi el final de la pantalla
+        ease: "none",
+        scrollTrigger: {
+            trigger: "body",
+            start: "top top",
+            end: "bottom bottom",
+            scrub: 1,
+            markers: false
+        }
+    });
 });
